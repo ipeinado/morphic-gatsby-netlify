e04e5daced15944946412f54ada5f6b5554d68e3
ipeinado <i.peinado.martinez@gmail.com>
2018-10-25 15:56:00 +0200

    added favicons

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: `Morphic`,
+    description: `News and docs about Morphic`,
+    siteUrl: `https://morphic.world` 
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        version: '1.0',
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          favicons: true,
+          opengraph: true,
+          windows: true,
+        },
+      },
+    },
+  ],
+}
