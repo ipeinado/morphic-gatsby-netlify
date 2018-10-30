ae7531143a957c69ad560afaf7194eba80a65d43
ipeinado <i.peinado.martinez@gmail.com>
2018-10-30 16:39:06 +0100

    added react-icons

--- src/components/header.js
+++ src/components/header.js
@@ -1,19 +1,50 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-export default () => (
-	<StaticQuery
-		query={graphql`
-			query HeadingQuery {
-				site {
-					siteMetadata {
-						title
+import { FaBars } from 'react-icons/fa'
+
+class Header extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			expanded: false,
+			minified: false,
+		}
+		this.handleClick = this.handleClick.bind(this)
+	}
+
+	componentDidMount() {
+		console.log("Component Did Mount")
+	}
+
+	handleClick = (e) => {
+		e.preventDefault()
+		console.log("Someone clicked the button")
+	}
+
+	render() {
+		return (
+			<StaticQuery
+				query={graphql`
+					query HeadingQuery {
+						site {
+							siteMetadata {
+								title
+							}
+						}
 					}
-				}
-			}
-		`}
-		render={data => (
-			<h1>{ data.site.siteMetadata.title }</h1>
-		)}
-	/>
-)
+				`}
+				render={data => (
+					<header>
+						<h1>{ data.site.siteMetadata.title }</h1>
+						<button type="button" data-toggle="collapse" onClick={ this.handleClick }>
+							<FaBars style={{ color: `red` }} />
+						</button>
+					</header>
+				)}
+			/>
+		)
+	}
+}
+
+export default Header
